718cea658a3043f1a0c9129d83bb1daa549fc895
piguycs <piguycs@pm.me>
2024-12-17 19:44:11 +0100

    part 2 todo

--- day-17/src/bin/part2.rs
+++ day-17/src/bin/part2.rs
@@ -7,5 +7,9 @@ fn main() {
 
 #[test]
 fn test() {
+    let input = include_str!("../../input0.txt");
+    let mut program = Program::parse(input).expect("invalid input provided");
+    program.code.reverse();
+
     todo!();
 }
