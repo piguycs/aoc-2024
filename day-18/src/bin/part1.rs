07dabbe583433f247beda09f8cd19d0469e7f107
piguycs <piguycs@pm.me>
2024-12-18 13:32:21 +0100

    day 18 part 1

--- day-18/src/bin/part1.rs
+++ day-18/src/bin/part1.rs
@@ -1,15 +1,23 @@
 use day_18::*;
 
 fn main() {
-    let input = include_str!("../../input0.txt");
-    parse(input, 70, 1024);
+    let input = include_str!("../../input1.txt");
+    let board = Board::parse(input, 70, 1024);
+
+    let ans = board.traverse();
+
+    println!("part 1: {ans}");
 }
 
 #[test]
 fn test() {
     let input = include_str!("../../input0.txt");
 
-    let board = parse(input, 6, 22);
+    let board = Board::parse(input, 6, 12);
 
-    assert_eq!(board.corruption.len(), 22);
+    assert_eq!(board.corruption.len(), 12);
+
+    let score = board.traverse();
+
+    assert_eq!(score, 22);
 }
--- day-18/src/lib.rs
+++ day-18/src/lib.rs
@@ -1,29 +1,66 @@
-use glam::UVec2;
+use glam::IVec2;
 use itertools::Itertools;
+use pathfinding::prelude::*;
 
 #[derive(Debug, Clone)]
 pub struct Board {
-    pub corruption: Vec<UVec2>,
-    pub start: UVec2,
-    pub end: UVec2,
+    pub corruption: Vec<IVec2>,
+    pub start: IVec2,
+    pub end: IVec2,
+    pub max: u32,
 }
 
-pub fn parse(input: &str, size: u32, steps: u32) -> Board {
-    let corruption = input
-        .lines()
-        .take(steps as usize)
-        .map(|e| e.split(",").collect_vec())
-        .map(|e| {
-            let x: u32 = e[0].parse().expect("non digit detected");
-            let y: u32 = e[1].parse().expect("non digit detected");
+impl Board {
+    pub fn parse(input: &str, size: u32, steps: u32) -> Board {
+        let corruption = input
+            .lines()
+            .take(steps as usize)
+            .map(|e| e.split(",").collect_vec())
+            .map(|e| {
+                let x: i32 = e[0].parse().expect("non digit detected");
+                let y: i32 = e[1].parse().expect("non digit detected");
 
-            UVec2::new(x, y)
-        })
-        .collect_vec();
+                IVec2::new(x, y)
+            })
+            .collect_vec();
 
-    Board {
-        corruption,
-        start: UVec2::new(0, 0),
-        end: UVec2::new(size, size),
+        Board {
+            corruption,
+            start: IVec2::new(0, 0),
+            end: IVec2::new(size as i32, size as i32),
+            max: size,
+        }
+    }
+
+    pub fn in_range(&self, pos: IVec2) -> bool {
+        let max = self.max as i32;
+        (0..=max).contains(&pos.x) && (0..=max).contains(&pos.y)
+    }
+
+    pub fn successor(&self, pos: IVec2) -> Vec<(IVec2, u32)> {
+        let neighbours = [IVec2::X, IVec2::NEG_X, IVec2::Y, IVec2::NEG_Y]
+            .iter()
+            .filter_map(|e| {
+                let neighbour = pos + e;
+
+                if self.in_range(neighbour) {
+                    Some((neighbour, 1))
+                } else {
+                    None
+                }
+            })
+            .filter(|(e, _)| !self.corruption.contains(e))
+            .collect_vec();
+
+        //
+
+        neighbours
+    }
+
+    pub fn traverse(&self) -> u32 {
+        let a = dijkstra(&self.start, |&e| self.successor(e), |&e| e == self.end)
+            .expect("dijkstra did not find any paths");
+
+        a.1
     }
 }
