afc20c26322116ca294744ab142b226122bd0c50
piguycs <piguycs@pm.me>
2024-12-23 14:11:13 +0100

    checkpoint

--- day-22/src/bin/part2.rs
+++ day-22/src/bin/part2.rs
@@ -1,3 +1,28 @@
+#![allow(unused)]
+
+use itertools::Itertools;
+
+use day_22::*;
+
 fn main() {
-    todo!("part 2");
+    let input = include_str!("../../input.txt");
+    let nums = get_nums(input);
+
+    for num in nums {
+        let mut secret = Secret::new(num);
+
+        let result = secret
+            .take(2000)
+            .map(|num| num % 10)
+            .tuple_windows()
+            .map(|(prev, curr)| (curr as isize - prev as isize, curr))
+            .tuple_windows()
+            .map(|(a, b, c, d)| {})
+            .collect_vec();
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
 }
