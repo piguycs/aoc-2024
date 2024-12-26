c61abfbc5e268660d9b201700e31c0ef6950c9ad
piguycs <piguycs@pm.me>
2024-12-26 18:16:47 +0100

    day-08 updated template

---
+++ day-08/src/bin/part1.rs
@@ -0,0 +1,8 @@
+fn main() {
+    todo!("part 1");
+}
+
+#[test]
+fn test() {
+    todo!("part 1 test");
+}
---
+++ day-08/src/bin/part2.rs
@@ -0,0 +1,8 @@
+fn main() {
+    todo!("part 2");
+}
+
+#[test]
+fn test() {
+    todo!("part 2 test");
+}
