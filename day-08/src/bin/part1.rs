21337578cfcfd1e8a02264d84e4470fe92175784
piguycs <piguycs@pm.me>
2024-12-30 10:41:25 +0100

    checkpoint

--- day-08/src/bin/part1.rs
+++ day-08/src/bin/part1.rs
@@ -1,8 +1,27 @@
-fn main() {
-    todo!("part 1");
-}
+use std::collections::HashMap;
 
-#[test]
-fn test() {
-    todo!("part 1 test");
+use glam::{ivec2, IVec2};
+
+fn main() {
+    let input = include_str!("../../input0.txt");
+
+    let width = input.lines().next().map(|e| e.chars().count()).unwrap();
+    let height = input.lines().count();
+
+    let antennas: HashMap<char, Vec<IVec2>> = input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars().enumerate().filter_map(move |(x, c)| {
+                if c.is_ascii_alphanumeric() {
+                    Some((c, ivec2(x as i32, y as i32)))
+                } else {
+                    None
+                }
+            })
+        })
+        .fold(HashMap::new(), |mut acc, (c, pos)| {
+            acc.entry(c).or_default().push(pos);
+            acc
+        });
 }
