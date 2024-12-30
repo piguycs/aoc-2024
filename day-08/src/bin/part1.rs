49c58f41c899a5e46504453532778b90183d0e3f
piguycs <piguycs@pm.me>
2024-12-30 11:54:41 +0100

    day 8 part 1

--- day-08/src/bin/part1.rs
+++ day-08/src/bin/part1.rs
@@ -1,12 +1,13 @@
 use std::collections::HashMap;
 
 use glam::{ivec2, IVec2};
+use itertools::Itertools;
 
 fn main() {
-    let input = include_str!("../../input0.txt");
+    let input = include_str!("../../input1.txt");
 
-    let width = input.lines().next().map(|e| e.chars().count()).unwrap();
-    let height = input.lines().count();
+    let width = input.lines().next().map(|e| e.chars().count()).unwrap() as i32;
+    let height = input.lines().count() as i32;
 
     let antennas: HashMap<char, Vec<IVec2>> = input
         .lines()
@@ -24,4 +25,21 @@ fn main() {
             acc.entry(c).or_default().push(pos);
             acc
         });
+
+    let ans = antennas
+        .values()
+        .flat_map(|positions| {
+            positions
+                .iter()
+                .combinations(2)
+                .flat_map(|poss| {
+                    let spacing = poss[0] - poss[1];
+                    [poss[0] + spacing, poss[1] - spacing]
+                })
+                .filter(|&IVec2 { x, y }| (0..width).contains(&x) && (0..height).contains(&y))
+        })
+        .unique()
+        .count();
+
+    println!("{ans}");
 }
