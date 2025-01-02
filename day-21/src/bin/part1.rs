fea8b898e6a5e09ba853f19d2009d6e26d2f2018
piguycs <piguycs@pm.me>
2025-01-02 23:19:41 +0100

    PART 1 DONE!!!!!

--- day-21/src/bin/part1.rs
+++ day-21/src/bin/part1.rs
@@ -1,47 +1,8 @@
-#![allow(unused)]
-
 use day_21::*;
 
 fn main() {
-    let input = include_str!("../../input1.txt");
-
-    let moves = NUMPAD.get_moves_all("029A");
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[rstest::rstest]
-    #[case("029A", 12)]
-    #[case("980A", 12)]
-    #[case("179A", 14)]
-    #[case("456A", 12)]
-    #[case("379A", 14)]
-    fn move_numpad(#[case] num: &str, #[case] move_len: usize) {
-        assert_eq!(NUMPAD.get_moves(num).len(), move_len);
-    }
-
-    #[rstest::rstest]
-    fn dirpad_memory_gen() {
-        assert_eq!(DIRPAD.mem.len(), 25)
-    }
-
-    // idk about other test cases
-    #[rstest::rstest]
-    #[case("029A", 28, 68)]
-    fn dirpad_traverse(#[case] num: &str, #[case] len1: usize, #[case] len2: usize) {
-        let moves = NUMPAD.get_moves_all(num);
-        //dbg!(&moves);
-
-        let mut moves = NUMPAD.get_moves_all(num);
-        let min_len = moves.iter().map(|v| v.len()).min().unwrap_or(0);
-        moves.retain(|v| v.len() == min_len);
-
-        let mut moves = DIRPAD.traverse_seq_all(moves[0].clone());
-
-        assert_eq!(moves.len(), len1);
-
-        todo!();
-    }
+    println!(
+        "{}",
+        solve(include_str!("../../input1.txt").lines().collect())
+    );
 }
--- day-21/src/lib.rs
+++ day-21/src/lib.rs
@@ -1,81 +1,275 @@
-#![allow(unused)]
+use glam::IVec2;
+use std::collections::HashMap;
 
-mod dirpad;
-mod numpad;
+pub const DEPTH: usize = 3;
 
-use std::{collections::HashMap, sync::LazyLock};
-
-use glam::{ivec2, IVec2};
-use itertools::Itertools;
-use pathfinding::prelude::*;
-
-use dirpad::*;
-use numpad::*;
-
-pub static NUMPAD: LazyLock<NumPad> = LazyLock::new(NumPad::default);
-pub static DIRPAD: LazyLock<DirPad> = LazyLock::new(DirPad::default);
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub enum Dir {
-    Left,
-    Right,
-    Up,
-    Down,
-    Action,
+#[derive(Debug, Clone, Copy)]
+#[allow(unused)]
+pub struct Key {
+    key: char,
+    pos: IVec2,
 }
 
-impl Dir {
-    pub const ALL: [Self; 5] = [Self::Left, Self::Right, Self::Up, Self::Down, Self::Action];
+impl Key {
+    pub fn new(key: char, x: i32, y: i32) -> Self {
+        Self {
+            key,
+            pos: IVec2 { x, y },
+        }
+    }
 }
 
-impl DirPad {
-    pub fn at(&self, pos: IVec2) -> Option<Dir> {
-        self.keypad
-            .iter()
-            .find(|&&(_, coord)| coord == pos)
-            .map(|&(e, _)| e)
+#[derive(Debug, Clone, Default)]
+struct KeyPad {
+    keys: HashMap<char, Key>,
+}
+
+impl KeyPad {
+    pub fn new() -> Self {
+        let mut pad = KeyPad::default();
+
+        pad.insert('0', 3, 1);
+        pad.insert('A', 3, 2);
+
+        pad.insert('1', 2, 0);
+        pad.insert('2', 2, 1);
+        pad.insert('3', 2, 2);
+
+        pad.insert('4', 1, 0);
+        pad.insert('5', 1, 1);
+        pad.insert('6', 1, 2);
+
+        pad.insert('7', 0, 0);
+        pad.insert('8', 0, 1);
+        pad.insert('9', 0, 2);
+
+        pad.insert('^', 0, 1);
+        pad.insert('B', 0, 2);
+
+        pad.insert('<', 1, 0);
+        pad.insert('v', 1, 1);
+        pad.insert('>', 1, 2);
+
+        pad
     }
 
-    pub fn ta(&self, dir: Dir) -> IVec2 {
-        self.keypad
-            .iter()
-            .find(|&&(curr, _)| curr == dir)
-            .map(|&(_, e)| e)
-            .unwrap()
+    pub fn insert(&mut self, key: char, x: i32, y: i32) {
+        self.keys.insert(key, Key::new(key, x, y));
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+struct Solution {
+    graph: HashMap<char, Vec<char>>,
+    path_memo: HashMap<String, Vec<String>>,
+    keypad_map: HashMap<char, Key>,
+}
+
+impl Solution {
+    fn calculate_len(&mut self) -> HashMap<String, usize> {
+        let mut len_map = HashMap::new();
+
+        let sources = ['>', 'v', '<', 'B', '^'];
+        let targets = ['>', 'v', '<', 'B', '^'];
+
+        for level in 1..=DEPTH {
+            for source in sources {
+                for target in targets {
+                    let key = Self::form_key(source, target, level as i32);
+                    let moves = self.find_and_translate(source, target);
+
+                    if level == 1 {
+                        len_map.insert(key, moves[0].len());
+                    } else {
+                        let mut shortest_len = usize::MAX;
+                        for m in &moves {
+                            let mut source2 = 'B';
+                            let mut move_len = 0;
+
+                            for target2 in m.chars() {
+                                let prev_key = Self::form_key(source2, target2, (level - 1) as i32);
+                                move_len += len_map.get(&prev_key).unwrap();
+                                source2 = target2;
+                            }
+
+                            shortest_len = shortest_len.min(move_len);
+                        }
+
+                        len_map.insert(key, shortest_len);
+                    }
+                }
+            }
+        }
+
+        len_map
     }
 
-    pub fn traverse_seq(&self, mut seq: Vec<Dir>) -> Vec<Dir> {
-        seq.insert(0, Dir::Action); // initial position
+    fn find_and_translate(&mut self, source: char, target: char) -> Vec<String> {
+        let key = format!("{}{}", source, target);
+        if let Some(moves) = self.path_memo.get(&key) {
+            moves.clone()
+        } else {
+            let moves = self.translate_paths(self.find_all_paths(source, target));
+            self.path_memo.insert(key, moves.clone());
+            moves
+        }
+    }
 
-        let mem = &self.mem; // HashMap: (start, end), Vec of direction
-        let mt = &Vec::new();
+    fn translate_paths(&self, paths: Vec<Vec<char>>) -> Vec<String> {
+        let mut list = vec![];
 
-        let vecs = seq
+        for path in paths {
+            if path.is_empty() {
+                continue;
+            }
+
+            let mut source = path[0];
+            let mut moves = String::new();
+
+            for target in path.iter().skip(1) {
+                moves.push(self.get_moves(source, *target));
+                source = *target;
+            }
+
+            moves.push('B');
+            list.push(moves);
+        }
+
+        list
+    }
+
+    #[allow(clippy::comparison_chain, clippy::needless_return)]
+    fn get_moves(&self, source: char, target: char) -> char {
+        let source_keypad = self.keypad_map.get(&source).unwrap();
+        let target_keypad = self.keypad_map.get(&target).unwrap();
+
+        let horizontal = target_keypad.pos.y - source_keypad.pos.y;
+        if horizontal > 0 {
+            return '>';
+        } else if horizontal < 0 {
+            return '<';
+        }
+
+        let vertical = target_keypad.pos.x - source_keypad.pos.x;
+        if vertical > 0 {
+            return 'v';
+        } else {
+            return '^';
+        }
+    }
+
+    fn find_all_paths(&self, start: char, end: char) -> Vec<Vec<char>> {
+        let mut all_paths = vec![];
+        let mut current_path = vec![];
+        let mut visited = HashMap::new();
+
+        self.dfs(start, end, &mut visited, &mut current_path, &mut all_paths);
+
+        let shortest_len = all_paths
             .iter()
-            .tuple_windows()
-            .map(|(&start, &end)| mem.get(&(start, end)).unwrap().first().unwrap_or(mt))
-            .collect_vec();
+            .map(|path| path.len())
+            .min()
+            .unwrap_or(usize::MAX);
 
-        vecs.iter()
-            .flat_map(|v| v.iter().chain([&Dir::Action]))
-            .cloned()
+        all_paths
+            .into_iter()
+            .filter(|path| path.len() == shortest_len)
             .collect()
     }
 
-    pub fn traverse_seq_all(&self, mut seq: Vec<Dir>) -> Vec<Vec<Dir>> {
-        seq.insert(0, Dir::Action); // initial position
+    fn dfs(
+        &self,
+        current: char,
+        end: char,
+        visited: &mut HashMap<char, bool>,
+        current_path: &mut Vec<char>,
+        all_paths: &mut Vec<Vec<char>>,
+    ) {
+        current_path.push(current);
+        visited.insert(current, true);
 
-        let mem = &self.mem;
+        if current == end {
+            all_paths.push(current_path.clone());
+        } else if let Some(neighbors) = self.graph.get(&current) {
+            for &neighbor in neighbors {
+                if !visited.get(&neighbor).unwrap_or(&false) {
+                    self.dfs(neighbor, end, visited, current_path, all_paths);
+                }
+            }
+        }
 
-        let vecs = seq
-            .iter()
-            .tuple_windows()
-            .map(|(&start, &end)| mem.get(&(start, end)).cloned().unwrap_or(vec![]))
-            .collect_vec();
-
-        let a = numpad::combine_directions(&vecs, 0, vec![]);
-        dbg!(&a);
-
-        a
+        current_path.pop();
+        visited.insert(current, false);
     }
+
+    fn form_key(source: char, target: char, level: i32) -> String {
+        format!("{}{}{:02}", source, target, level)
+    }
+}
+
+pub fn solve(data: Vec<&str>) -> usize {
+    let keypad = KeyPad::new();
+
+    let mut graph = HashMap::new();
+    graph.insert('A', vec!['0', '3']);
+    graph.insert('0', vec!['A', '2']);
+    graph.insert('3', vec!['A', '2', '6']);
+    graph.insert('2', vec!['1', '5', '3', '0']);
+    graph.insert('1', vec!['4', '2']);
+    graph.insert('6', vec!['9', '5', '3']);
+    graph.insert('5', vec!['8', '4', '2', '6']);
+    graph.insert('4', vec!['7', '5', '1']);
+    graph.insert('9', vec!['8', '6']);
+    graph.insert('8', vec!['7', '5', '9']);
+    graph.insert('7', vec!['4', '8']);
+    graph.insert('>', vec!['B', 'v']);
+    graph.insert('v', vec!['>', '^', '<']);
+    graph.insert('<', vec!['v']);
+    graph.insert('B', vec!['^', '>']);
+    graph.insert('^', vec!['B', 'v']);
+
+    let mut solution = Solution {
+        graph,
+        path_memo: HashMap::default(),
+        keypad_map: keypad.keys,
+    };
+
+    let len_map = solution.calculate_len();
+    let mut source = 'A';
+    let mut total = 0;
+
+    for (i, data) in data.iter().enumerate() {
+        let number: usize = data[0..3].parse().expect("Invalid number format");
+        let mut total_length = 0;
+
+        for target in data.chars() {
+            let moves = solution.find_and_translate(source, target);
+            let mut shortest_length = usize::MAX;
+
+            for move_seq in moves {
+                let mut move_length = 0;
+                let mut source2 = 'B';
+
+                for target2 in move_seq.chars() {
+                    let key = Solution::form_key(source2, target2, (DEPTH - 1) as i32);
+                    move_length += len_map.get(&key).expect("Key not found in length map");
+                    source2 = target2;
+                }
+
+                shortest_length = shortest_length.min(move_length);
+            }
+
+            println!(
+                "source={} target={} shortestLength={}",
+                source, target, shortest_length
+            );
+            total_length += shortest_length;
+            source = target;
+        }
+
+        println!("dataArray[{}]={} totalLength = {}", i, data, total_length);
+        total += number * total_length;
+    }
+
+    total
 }
